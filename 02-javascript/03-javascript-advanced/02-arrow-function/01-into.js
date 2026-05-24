dfc59153533c9440b14782e71a4cb0b6a7a959b7
Hili <sy1253079@gmail.com>
2026-05-24 20:06:57 +0300

    lesson 10 first

---
+++ 02-javascript/03-javascript-advanced/02-arrow-function/01-into.js
@@ -0,0 +1,25 @@
+const addition = function(num1, num2) {
+    return num1 + num2
+}
+
+console.log(addition(8, 7));
+
+
+const arrowAddition = (num1, num2) => {
+    console.log("I'm inssige arrow");    
+    return num1 + num2
+}
+
+console.log(arrowAddition(8, 8));
+
+
+const shortArrowAdditio = (num1, num2) => num1 + num2
+
+console.log(shortArrowAdditio(8, 9));
+
+
+const square = num => num * num
+
+console.log(square(7));
+
+
